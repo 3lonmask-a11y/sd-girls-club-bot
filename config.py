f330e9543a849f83705e4382b5658c69d91d55d6
3lonmask-a11y <3lonmask@gmail.com>
2025-11-10 06:40:31 +0300

    Update bot settings and configuration values

--- config.py
+++ config.py
@@ -4,16 +4,16 @@ from typing import Tuple
 @dataclass
 class Settings:
     # Токен бота из BotFather
-    BOT_TOKEN: str = "ВАШ_TELEGRAM_BOT_TOKEN"
+    BOT_TOKEN: str = "8344740572:AAFgGaZl0Bu-CFc05V3TKFYNIdEeKpAEs94"
 
     # ID админов, которые могут вызывать /set_sub /stats и т.п.
-    ADMINS: Tuple[int, ...] = (123456789,)
+    ADMINS: Tuple[int, ...] = (931831277,)
 
     # Куда слать запросы "связаться с куратором" и ответы на чек-ин
-    ADMIN_CHAT_ID: int = 123456789
+    ADMIN_CHAT_ID: int = 931831277
 
     # Ссылки — потом подставишь свои
-    SUBSCRIPTION_LINK: str = "https://example.com/pay"
+    SUBSCRIPTION_LINK: str = "https://b2b.cbrpay.ru/BS1B0015GJF90OHV8P4B4JO82PGHN395"
     MATERIALS_LINK: str = "https://t.me/your_materials_channel"
     SEASONS_LINK: str = "https://example.com/seasons"
 
@@ -22,3 +22,4 @@ class Settings:
     DATA_FILE: str = "data/users.json"
 
 settings = Settings()
+
